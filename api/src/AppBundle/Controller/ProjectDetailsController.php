2f4515203962f6804bec5b72b8c7da66b6356ab0
Adrian Pietka <adrian@pietka.me>
2017-04-30 12:39:34 +0200

    Project details

---
+++ api/src/AppBundle/Controller/ProjectDetailsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Query\GetProjectQuery;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProjectDetailsController extends AppController
+{
+    /**
+     * @Route("/project/{id}", name="project_details", requirements={"id": "\d+"})
+     * @Method("GET")
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function indexAction(int $id) : JsonResponse
+    {
+        $project = $this->queryDispatcher()->execute(new GetProjectQuery((int)$id));
+
+        return $this->json($project, Response::HTTP_OK);
+    }
+}
---
+++ api/src/AuditorBundle/Entity/FileContentEntity.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apietka
+ * Date: 2017-04-29
+ * Time: 21:32
+ */
