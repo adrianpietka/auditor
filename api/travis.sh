5bb082ba502c00a9016970e2dfcbbc3edf8a6dad
Adrian Pietka <adrian@pietka.me>
2017-05-11 11:05:56 +0200

    Setup exit code for travis.sh

--- api/travis.sh
+++ api/travis.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-composer install --prefer-source --no-interaction --dev
+STATUS=0
+
 ./vendor/bin/phpcs --standard=PSR2 --encoding=utf-8 src/ tests/
+[ $? == 0 ] || STATUS=1
+
 composer unit
+[ $? == 0 ] || STATUS=1
+
 composer integration
+[ $? == 0 ] || STATUS=1
+
+exit $STATUS
